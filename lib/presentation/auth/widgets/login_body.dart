300afc9ecf1d8f280b48a85aa6fda5e3012b9672
Vlad Edelev <vlladedellev@gmail.com>
2023-10-08 19:49:32 +0300

    feature: moved shared widgets to the core folder

--- lib/presentation/auth/widgets/login_body.dart
+++ lib/presentation/auth/widgets/login_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_app/core/utils/colors.dart';
-import 'package:flutter_auth_app/presentation/shared_widgets/custom_button.dart';
-import 'package:flutter_auth_app/presentation/shared_widgets/custom_text_field.dart';
+import 'package:flutter_auth_app/core/shared_widgets/custom_button.dart';
+import 'package:flutter_auth_app/core/shared_widgets/custom_text_field.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginBody extends StatelessWidget {
